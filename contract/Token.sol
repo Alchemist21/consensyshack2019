ed611b938b98d4224f216ded69839c70a3b9dbcf
jameshih <j@shih.app>
2019-07-20 16:53:45 -0400

    push token contract

---
+++ contract/Token.sol
@@ -0,0 +1,49 @@
+pragma solidity ^0.5.0;
+
+contract Token {
+   address public owner;
+   
+   uint public total_supply ;
+
+   mapping(address => uint) public  balance;
+
+   modifier isOwner(){
+       require(msg.sender == owner);
+       _;
+   } 
+   
+   modifier notNull(address x){
+       require(x != address(0x0));
+       _;
+   }
+
+   constructor() public{
+       owner = msg.sender;
+   }
+
+   function _mint(address x, uint amount) external isOwner notNull(x){
+       
+       uint _temp =  balance[x];
+       uint _supply = total_supply;
+       balance[x] = _temp + amount;
+       total_supply = _supply + amount;
+   }
+   
+   function _burn(address x, uint amount) external isOwner notNull(x){
+       require(balance[x] >= amount);
+       uint _temp = balance[x];
+       uint _supply = total_supply;
+       balance[x] = _temp - amount;
+       total_supply = _supply - amount;   
+       
+   }
+   
+   function transfer(address recipient, uint amount) external notNull(recipient){
+       require(balance[msg.sender] >= amount);
+       uint _recipient_amount = balance[recipient];
+       uint _sender_amount = balance[msg.sender];
+       balance[msg.sender] = _sender_amount - amount;
+       balance[recipient] = _recipient_amount + amount;
+   }
+
+}
